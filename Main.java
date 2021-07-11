173116b6afe0984277a24ff6f1da01b45df29646
Cihan Yıldız <69516827+chn-yldz@users.noreply.github.com>
2021-07-11 23:31:11 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir Sayi Giriniz :");
+        int n1=scanner.nextInt();
+        int n2=(int)(n1/2)+1;
+
+        for(int i=1;i<=n2;i++)
+        {
+            for(int j=1;j<=n2-i;j++)
+                System.out.print(" ");
+
+            for(int j=1;j<=2*i-1;j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+        for(int i=n2-1;i>=1;i--)
+        {
+            for(int j=1;j<=n2-i;j++)
+                System.out.print(" ");
+
+            for(int j=1;j<=2*i-1;j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
